75a435cca1bb42717618669fc66b1eac47febc53
Wasim Hyder <wasim@creativefaze.com>
2023-11-11 21:42:16 +0500

    Added error handling with loader

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,26 @@
+import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent'
+import { useRouteError } from 'react-router-dom';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  let title = 'An error occurred!';
+  let message = 'Something went wrong.';
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not found the page.';
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  )
+}
+export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,6 +4,9 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+  // if (data.isError) {
+  //   return <p>{data.message}</p>
+  // }
   const events = data.events;
   return <EventsList events={events} />
 }
@@ -14,7 +17,11 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    // An approach to throw errow with loader.
+    // return { isError: true, message: 'Could not fetch events. ' }
+
+    //Below is an alternative approach
+    throw new Response(JSON.stringify({ message: 'Could not fetch the data.' }), { status: 500 });
   } else {
     return response;
   }
