88eb9ebf2bdd1dacb69ab5adbb0d12861fdd2d5c
Wasim Hyder <wasim@creativefaze.com>
2023-11-13 21:53:51 +0500

    Loaded and displayed details of event

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,11 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from '../components/EventItem'
 
 const EventDetailPage = () => {
-  const params = useParams();
+  const data = useLoaderData();
   return <>
-    <h1>Event Detail Page</h1>
-    <h2>{params.id}</h2>
+    <EventItem event={data.event} />
   </>
 };
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const eventId = params.id;
+  const response = await fetch('http://localhost:8080/events/' + eventId);
+  if (!response.ok) {
+    throw json({ message: 'Could not fetch details for selected event.' }, {
+      status: 500
+    })
+  }
+  else {
+    return response;
+  }
+}
