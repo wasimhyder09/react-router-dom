e7a1f5b7e8440b555e5e3233f55819c3a33c1482
Wasim Hyder <wasim@creativefaze.com>
2023-11-14 23:37:00 +0500

    Added data defer

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,19 +1,21 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
-  // if (data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
-  return <EventsList events={events} />
+  const { events } = useLoaderData();
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadEvents) => <EventsList events={loadEvents} />}
+      </Await>
+    </Suspense >
+  );
 }
 
 export default EventsPage;
-
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
@@ -29,6 +31,12 @@ export async function loader() {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+export function loader() {
+  return defer({
+    events: loadEvents()
+  });
 }
